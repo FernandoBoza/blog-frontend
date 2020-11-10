0869dd155ed160aac557b4379441241fa95c338d
fernandoboza <hi@fernandoboza.com@users.noreply.github.com>
2020-11-10 03:44:17 -0500

    added url directly

--- apollo/apollo.tsx
+++ apollo/apollo.tsx
@@ -4,7 +4,7 @@ import withApollo from "next-with-apollo";
 import { createHttpLink } from "apollo-link-http";
 import fetch from "isomorphic-unfetch";
 
-const GRAPHQL_URL = process.env.API_URL || "http://localhost:1337";
+const GRAPHQL_URL = process.env.API_URL || "https://fb-cms.herokuapp.com";
 
 const link = createHttpLink({
     fetch,
