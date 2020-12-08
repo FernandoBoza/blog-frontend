08b6603f5c2c1c281b00d5bdb4390da37e6ba5fb
fernandoboza <hi@fernandoboza.com@users.noreply.github.com>
2020-12-08 01:56:41 -0500

    cleaned up card cmpomnent

--- components/Card.component.tsx
+++ components/Card.component.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Link from "next/link";
 import { publishDate } from '../utils/Dates'
 import { shortenContent } from '../utils/SharedPlans'
