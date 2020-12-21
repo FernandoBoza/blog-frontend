620b55c85062247386a6c74243e4a1be32257f48
fernandoboza <hi@fernandoboza.com@users.noreply.github.com>
2020-12-21 00:55:36 -0500

    trying google analytics one more time

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6MV56X9TG9"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', 'G-6MV56X9TG9');
+                                `
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html >
+        );
+    }
+}
